59733ad5d0a7bceaeb203f69ff04bba53c772a37
yourprettygood <didur.danil@bk.ru>
2023-12-07 16:29:40 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1Задача 1. Дано целое число n, указывающее номер дня недели от 1 до 7. По указанному номеру n вывести название соответствующего дня недели.
+
+
+
+            //Console.Write("Введите число ");
+            //byte day = byte.Parse(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine("Понедельник "); break;
+            //    case 2: Console.WriteLine("Вторник "); break;
+            //    case 3: Console.WriteLine("Среда "); break;
+            //    case 4: Console.WriteLine("Четверг "); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суббота"); break;
+            //    case 7: Console.WriteLine("Воскресенье "); break;
+            //}
+            //if (day > 7)
+            //{
+            //    Console.Write("Число должно быть от 1 до 7 ");
+            //}
+            //Console.ReadLine();
+
+
+            //#2
+            //Задача 2. В зависимости от порядкового номера месяца (1, 2, ... , 12) вывести на экран его название (январь, февраль, ... , декабрь).
+
+
+            //Console.Write("Введите месяц ");
+            //byte month = byte.Parse(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 1: Console.WriteLine("Январь"); break;
+            //    case 2: Console.WriteLine("Февраль"); break;
+            //    case 3: Console.WriteLine("Март"); break;
+            //    case 4: Console.WriteLine("Апрель"); break;
+            //    case 5: Console.WriteLine("Май"); break;
+            //    case 6: Console.WriteLine("Июнь"); break;
+            //    case 7: Console.WriteLine("Июль"); break;
+            //    case 8: Console.WriteLine("Август"); break;
+            //    case 9: Console.WriteLine("Сентябрь"); break;
+            //    case 10: Console.WriteLine("Октябрь"); break;
+            //    case 11: Console.WriteLine("Ноябрь"); break;
+            //    case 12: Console.WriteLine("Декабрь"); break;
+            //    default: Console.WriteLine("Введите порядковый номер от 1-12"); break;
+            //}
+            //Console.ReadLine();
+
+
+            //№3
+            //Задача 3. По номеру месяца вывести название времени года.
+
+
+            //Console.Write("Введите номер месяца ");
+            //byte numb = byte.Parse(Console.ReadLine());
+            //switch (numb)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:Console.WriteLine("Зима");break;
+            //    case 3:
+            //    case 4: 
+            //    case 5: Console.WriteLine("Весна"); break;
+            //    case 6:
+            //    case 7:
+            //    case 8: Console.WriteLine("Лето"); break;
+            //    case 9:
+            //    case 10:
+            //    case 11: Console.WriteLine("Осень"); break;
+
+            //    default:Console.WriteLine("Введите число от 1 до 12");break;
+            //}
+
+            //№4
+            //Задача 4. Вывести название предмета по введенной первой букве: ф – физика, м – математика, и – история, г – география, б – биология.
+
+
+            //Console.Write("Введите первую букву предмета ");
+            //char letter = Convert.ToChar(Console.ReadLine());
+            //switch (letter)
+            //{
+            //    case 'ф':
+            //    case 'Ф': Console.WriteLine("Физика"); break;
+            //    case 'м':
+            //    case 'М': Console.WriteLine("Математика"); break;
+            //    case 'и':
+            //    case 'И': Console.WriteLine("История"); break;
+            //    case 'г':
+            //    case 'Г': Console.WriteLine("География"); break;
+            //    case 'б':
+            //    case 'Б': Console.WriteLine("Биология"); break;
+            //    default:Console.Write("Введите 1-ую букву одного из предложенных предметов");break;
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
